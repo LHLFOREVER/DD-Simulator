dd701a92a2e6263690157c6276a71e0edc7e6aa3
Kofwana <kofwanlawson@gmail.com>
2023-08-31 16:58:52 -0400

    Removed my API key

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 import openai
 
 # Set your OpenAI API key here
-openai.api_key = "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" #authentification
+openai.api_key = " " #authentification
 
 # Define a prompt that outlines the task for the model
 prompt = "You are ChatGPT, a helpful AI assistant. Answer the following questions:\n1. What is the capital of France?"
 
 # Use the OpenAI API to generate a response
 response = openai.Completion.create(
-    engine="text-davinci-003",  # Choose the engine you want to use
+    engine="text-davinci-003",  # engine selection
     prompt=prompt, # need to provide input
     max_tokens=50  # limiting the length of the generated response
 )
@@ -16,3 +16,4 @@ response = openai.Completion.create(
 # Process and print the generated response
 generated_text = response.choices[0].text
 print(generated_text)
+# had to remove my key and push again ahh
