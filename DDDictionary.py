0f252046b295a7ce141a7ec745e5976f86a9b226
Adithya S <aks6314@psu.edu>
2023-09-21 14:51:04 -0400

    Worked on Jobs

--- DDDictionary.py
+++ DDDictionary.py
@@ -1,28 +1,55 @@
 # Data Dictionary File
 
 Person = {
-    "Status":["Nobility", "Knight", "Civilian", "Criminal"]
-    "Job":[]
+    "Status":["Nobility", "Knight", "Civilian", "Criminal"],
+    "Affiliation":["Group", "Job"],
+    "Job":["Group", "Salary","Years Experience"]
 }
 
-Job = {
+Affiliation = {
+    "Group":["Farmer's Guild", "Merchant's Guild", "Labor Union", "Government", "Black Market", "Adventurer's Guild", "Culinary Union","Entertainment Union", "Smithy Guild", "Spiritual Union", "Treasury", "Medical Guild", "Researchers Union", "Teacher's Union", "Education Association", "Military", "Service Union"],
     
 }
+
+Group = {
+    "Farmer's Guild":[],
+    "Merchant's Guild":[],
+    "Labor Union":[],
+    "Government":[],
+    "Black Market":[],
+    "Adventurer's Guild":[],
+    "Culinary Union":[],
+    "Entertainment Union":[],
+    "Smithy Guild":[],
+    "Spiritual Union":[],
+    "Treasury":[],
+    "Medical Guild":[],
+    "Researchers Union":[],
+    "Teacher's Union":[],
+    "Education Association":[],
+    "Military":[],
+    "Service Union":[]
+
+
+
+}
+
 
 
 Status = {
     "Nobility":["Rank","Nobility Status"],
     "Knight": ["False", "True", "Rank"],
     "Civilian":["Civilian Status"],
-    "Criminal": ["False", "True"]
+    "Criminal": ["False", "True"],
+    "Adventurer": ["Adventurer Status", "Rank", "Role"]
 }
 
 Civilian  = {
-    "Civilian Status": ["False", "True"],
-    "Job" :["Milita", "Farmer","Laborer", "Merchant","Local Official", "Business Owner", "Adventurer"]
+    "Civilian Status": ["False", "True"]
 }
 
 Adventurer = {
+    "Adventurer Status": ["False", "True"],
     "Rank": ["Copper", "Iron","Bronze", "Silver", "Amethyst", "Gold", "Emerald", "Corundum", "Diamond", "Platinum"],
     "Role": ["Mage", "Rogue", "Brawler", "Archer", "Sniper"]
 }
