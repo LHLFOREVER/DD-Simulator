5db8b22c4cb43156cbee06ec44c4310a6eb69577
Adithya S <aks6314@psu.edu>
2023-10-26 13:48:00 -0400

    Added functionality to the combat and movement

--- DDDictionary.py
+++ DDDictionary.py
@@ -58,10 +58,37 @@ Nobility = {
 }
 
 
+Enemy = {
+    "Status":["Nobility", "Knight", "Civilian", "Criminal"],
+    "Affiliation":"Chaos",
+    "Attributes":["HP","STR","DEX","VIT","INT","SPD"],
+    "Species":["Wild_Mobs, Thinker_Mobs"],
+    "Level":1,
+    "Experience":0
+    
+}
+
+#Mobs 
+Wild_Mobs = {
+    "wild":["slime","goblin", "zombie", "orc", "construct", "skeleton"]
+}
+
+Thinker_Mobs = {
+    "thinker":["bandit", "robot", "robber", "villian's servant", "dragon", "vampire"]
+}
 
 
-# Environment
+#Weapons:
 
+Weapon = {
+    "sword":["shortsword", "longsword", "katana","broadsword", "rapier"],
+    "bow":["crossbow", "recurve bow", "long bow", "shortbow"],
+    "knife":["dagger", "knife"],
+    "heavy":["hammer", "club", "mace"],
+    "spear":["spear", "polearm"],
+    "wand":["wand", "staff"],
+    "gun":["pistol", "hunter rifle", "magic rifle"]
+}
 
 
 Environment = {
@@ -72,6 +99,28 @@ Environment = {
 LocSize = {
     "size":["Small", "Medium", "Large"],
 }
+
+#DOT is Damage Over Time
+BuffsnDebuffs = {
+    "speed": {"extra action", "extra movement", "warp", "acceleration"},
+    "slow": {"sluggish", "exhausted", "stun", "chained", "panic"}, 
+    "DOT": {"poison","frostbite","shock"},
+    "redirect":{"charm", "blind", "haze"},
+    "ally_debuff":{"betray","corpse explosion"},
+    "self_debuff": {"dull", "fragile", "vulnerable", "weak"},
+    "self_buff":{"absorption", "armor", "berserk", "bloodlust", "defense", "dodge", "elasiticity", "focus", "fury", "hatred", "immortality",
+                "immunity", "merciless", "mirror", "regen", "shell", "shield", "taunt", "thorn", "vampire"}
+}
+
+Multiplier = {
+    "buff":{"1.0x", "1.2x", "1.5x"},
+    "debuff":{"0.75x", "0.5x", "0.25x"} 
+}
+ 
+
+
+
+# Function Testing
 Floors = 0
 Rooms = 0
 
@@ -95,25 +144,29 @@ else:
     Floors = random.randint(1,3)
     Rooms = random.randint(1,3)
 
+Floors = 0
+Rooms = 0
+def location_build(place):
+    for place in Environment["Location"]:
+        if Environment["Location"] == ("Villian's Castle" or "Temple" or "Adventurer's Guild"):
+            LocSize["size"] = "Medium"
+        elif Environment["Location"] == ("Dark Cave" or "Dungeon"):
+            LocSize["size"] = "Large"
+        elif Environment["Location"] != ("Dark Cave" or "Dungeon" or "Villian's Castle" or "Temple" or "Adventurer's Guild"):
+            LocSize["size"] = "Small"
+        else:
+            return("Please enter a valid location.")
 
+# Determining Building Size
+    if LocSize["size"] == "Large":
+        Floors = random.randint(11,100)
+        Rooms = random.randint(11,100)
+    elif LocSize["size"] == "Medium":
+        Floors = random.randint(4,10)
+        Rooms = random.randint(4,10)
+    else:
+        Floors = random.randint(1,3)
+        Rooms = random.randint(1,3)
+    return Floors, Rooms
 
-# Enemies
-
-Enemy = {
-    "Status":["Nobility", "Knight", "Civilian", "Criminal"],
-    "Affiliation":"Chaos",
-    "Attributes":["HP","STR","DEX","VIT","INT","SPD"],
-    "Species":["Wild_Mobs, Thinker_Mobs"],
-    "Level":1,
-    "Experience":0
-    
-}
-
-
-Wild_Mobs = {
-    "wild":["slime","goblin", "zombie", "orc", "construct"]
-}
-
-Thinker_Mobs = {
-    "thinker":["bandit", "robot", "robber", "villian's servant"]
-}
+location_build("Temple")
